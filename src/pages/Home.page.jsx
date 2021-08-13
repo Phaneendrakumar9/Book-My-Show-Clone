38e2792cbec3b9be2e9aefd17b6ed00ef0404a52
Phaneendrakumar9 <phaneendrakumar116@gmail.com>
2021-08-13 16:43:34 +0530

    Premier carousal

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import EntertainmentCardSlider from "../components/Entertainment/Entertainmentcard.component";
+import Premier from "../components/Premier/Premier.component";
 
 const HomePage = () => {
     return <>
@@ -7,6 +8,7 @@ const HomePage = () => {
     <div className="container mx-auto px-4">
         <h1 className="text-2xl font-bold text-gray-800 my-3">The Best of Entertainment</h1>
     <EntertainmentCardSlider/>
+    <Premier/>
     </div>
     </>;
 };
