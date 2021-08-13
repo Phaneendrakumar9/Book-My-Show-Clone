6830ff580b167aa053895c0ba2b9ed588c562a31
Phaneendrakumar9 <phaneendrakumar116@gmail.com>
2021-08-13 13:18:45 +0530

    Added Hero Carousal

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import {Route} from "react-router-dom";
 
 //HOC
 import DefaultHOC from "../src/HOC/Default.HOC";
+
 import Temp from "./components/Temp";
 
-//Components
+
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 function App() {
   return (
--- src/components/HeroCarousal/HeroCarousal.component.jsx
+++ src/components/HeroCarousal/HeroCarousal.component.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import HeroSlider from "react-slick";
 
-// Import css files
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
 const HeroCarousal = () => {
 
     const settingsLG={
