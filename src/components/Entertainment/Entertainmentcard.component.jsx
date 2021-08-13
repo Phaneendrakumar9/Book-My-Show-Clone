fc8dfb789b96ec1083788163cb515bd8eb7f254c
Phaneendrakumar9 <phaneendrakumar116@gmail.com>
2021-08-13 16:09:12 +0530

    Entertainment carousal

--- src/components/Entertainment/Entertainmentcard.component.jsx
+++ src/components/Entertainment/Entertainmentcard.component.jsx
@@ -58,6 +58,7 @@ const EntertainmentCardSlider = () => {
 
   return (
     <>
+    
       <Slider {...settings}>
         {EntertainmentImage.map((image) => (
           <EntertainmentCard src={image} />
--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -3,7 +3,11 @@ import EntertainmentCardSlider from "../components/Entertainment/Entertainmentca
 
 const HomePage = () => {
     return <>
+    {""}
+    <div className="container mx-auto px-4">
+        <h1 className="text-2xl font-bold text-gray-800 my-3">The Best of Entertainment</h1>
     <EntertainmentCardSlider/>
+    </div>
     </>;
 };
 
