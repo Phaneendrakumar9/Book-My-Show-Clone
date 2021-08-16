4e2984fc5d5af6831801c26627c3f99d5cd3a10f
Phaneendrakumar9 <phaneendrakumar116@gmail.com>
2021-08-16 12:42:30 +0530

    Added Movie Hero Component

---
+++ src/components/MovieHero/MovieInfo.component.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const MovieInfo = () => {
+    return (
+        <>
+        <div className="flex flex-col gap-3 lg:gap-8">
+          <div className="flex items-center gap-3 md:px-4">
+            <div className="w-40 h-8">
+            <img src="https://in.bmscdn.com/moviemode/tvod/premiere-tag.png"
+            alt="Premier"
+            className="w-full h-full"/>
+            </div>  
+            <span className=" bg-bms-700 p-1 text-xs text-white rounded-md">Streaming Now</span>
+          </div>
+          <h1 className="text-white lg:text-5xl font-bold hidden lg:block">Zack Snyder's Justice League</h1>
+          <div className="flex flex-col-reverse gap-3 lg:gap-5 lg:flex-col">
+          <div className="text-white font-light flex flex-col gap-2 md:px-4">
+              <h4>4k &bull; English &bull; Action</h4>
+              <h4>3h 23m &bull; Action,Sci-Fi,Thriller &bull; 16+</h4>
+          </div>
+          <div className="flex items-center gap-3 md:px-4 md:w-screen lg:w-full">
+              <button className="bg-bms-100 w-full py-3 text-white font-semibold rounded-lg">
+                  Rent ₹149
+              </button>
+              <button className="bg-bms-100 w-full py-3 text-white font-semibold rounded-lg">
+                  Buy ₹649
+              </button>
+          </div>
+          </div>
+        </div> 
+        </>
+    )
+}
+
+export default MovieInfo;
