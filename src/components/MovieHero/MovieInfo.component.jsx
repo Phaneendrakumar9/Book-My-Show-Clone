95da66a728d652c95ccfa5d463aad2b6537e8042
Phaneendrakumar9 <phaneendrakumar116@gmail.com>
2021-08-18 14:13:02 +0530

    Added Razorpay Gateway

--- src/components/MovieHero/MovieInfo.component.jsx
+++ src/components/MovieHero/MovieInfo.component.jsx
@@ -1,14 +1,28 @@
-import React,{useContext}from 'react'
+import React,{useContext,useState}from 'react'
 import { MovieContext } from '../../context/movie.context';
+import PaymentModal from '../PaymentModal/Payment.component';
 
 const MovieInfo = () => {
-   const {movie} =useContext(MovieContext);
+    const [isOpen,setIsOpen]= useState(false); 
+    const [price,setPrice] = useState(0);
+    const {movie} =useContext(MovieContext);
 
    //Optional Chaining
    const genres = movie.genres?.map(({name})=>name).join(", ")
 
+   const rentMovies = ()=>{
+      setIsOpen(true);
+      setPrice(149);
+   };
+
+   const buyMovies = () =>{
+       setIsOpen(true);
+       setPrice(649);
+   }
+
     return (
         <>
+        <PaymentModal setIsOpen={setIsOpen} isOpen={isOpen} price={price}/>
         <div className="flex flex-col gap-3 lg:gap-8">
           <div className="flex items-center gap-3 md:px-4">
             <div className="w-40 h-8">
@@ -25,10 +39,10 @@ const MovieInfo = () => {
               <h4>{(movie.runtime/60).toFixed(2)} h &bull; {genres} &bull; 16+</h4>
           </div>
           <div className="flex items-center gap-3 md:px-4 md:w-screen lg:w-full">
-              <button className="bg-bms-100 w-full py-3 text-white font-semibold rounded-lg">
+              <button  onClick={rentMovies} className="bg-bms-100 w-full py-3 text-white font-semibold rounded-lg">
                   Rent ₹149
               </button>
-              <button className="bg-bms-100 w-full py-3 text-white font-semibold rounded-lg">
+              <button onClick={buyMovies} className="bg-bms-100 w-full py-3 text-white font-semibold rounded-lg">
                   Buy ₹649
               </button>
           </div>
